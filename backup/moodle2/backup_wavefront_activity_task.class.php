de76f8aa42a9685a577082d26b16419f8456592f
Ian Wild <ian.wild@heavyhorse.co.uk>
2017-06-18 20:58:13 +0100

    If script accessed from outside of Moodle then it should die.

--- backup/moodle2/backup_wavefront_activity_task.class.php
+++ backup/moodle2/backup_wavefront_activity_task.class.php
@@ -21,6 +21,9 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
+
 require_once($CFG->dirroot . '/mod/wavefront/backup/moodle2/backup_wavefront_stepslib.php');
 
 /**
