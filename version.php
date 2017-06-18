69a2d9e25a6df8acb89777053e7190954e0d6e2b
Ian Wild <ian.wild@heavyhorse.co.uk>
2017-06-18 21:08:37 +0100

    Updated version number accordingly.

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017061400;  // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version  = 2017061800;  // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2016052300;  // Requires this Moodle version.
 $plugin->cron     = 0;           // Period for cron to check this plugin (secs).
 
 $plugin->component = 'mod_wavefront';
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.0.0.4';
+$plugin->release = '0.0.0.5';
