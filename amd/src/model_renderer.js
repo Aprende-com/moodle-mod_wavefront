70a5055c01643117c23ab726b8d293ddb49572c4
Ian Wild <ianwild@luminaconsultancy.com>
2022-02-26 18:20:58 +0000

    Improvement: allow multiple models to be shown in a single view.

--- amd/src/model_renderer.js
+++ amd/src/model_renderer.js
@@ -31,113 +31,155 @@ import * as THREE from 'mod_wavefront/three';
 import { MTLLoader } from 'mod_wavefront/MTLLoader';
 import { OBJLoader } from 'mod_wavefront/OBJLoader';
 import { OrbitControls } from 'mod_wavefront/OrbitControls';
+import jQuery from 'jquery';
 
-let camera, controls, scene, renderer;
+var cameras = [], controls_array = [], scenes = [], renderers = [];
 
-var wavefront_stage;
-var stage_width, stage_height; 
-var lighting, ambient, keyLight, fillLight, backLight;
+var containers = [];
+var stage_widths = [], stage_heights = []; 
+var lightings = [], ambients = [], keyLights = [], fillLights = [], backLights = [];
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
 const animate = () => {
+	 renderers.forEach( function(renderer, i) {
+	    renderer.render(scenes[i], cameras[i]);
+	 	controls_array[i].update();
+	 });
+	 
 	 requestAnimationFrame(animate);
-     renderer.render(scene, camera);
-	 controls.update();
 }
 
-export const init = (stage, obj_file, mtl_file, baseurl, width, height, cameraangle, camerafar, camerax, cameray, cameraz) => {
-    
-    var container = document.getElementById(stage);
-        	        	
-	if (!WebGL.isWebGLAvailable() ) {
-		
+export const init = (stages) => {
+
+	if (!WebGL.isWebGLAvailable() ) {			
 	    const warning = WebGL.getWebGLErrorMessage();
 		document.getElementById( 'container' ).appendChild( warning );
 	    return true;
 	}
-			
-	wavefront_stage = stage;
-	stage_width = width;
-	stage_height = height;
+	
+	console.log(stages);
+	
+    stages.forEach(function(stage) {
+        
+    	var container = document.getElementById(stage);
+    	console.log(container);
+		containers.push(container);
 		
-	// Scene
-	scene = new THREE.Scene();
+		// Get stage attributes
+		var stage_width = jQuery(container).attr("data-stagewidth");
+		console.log(stage_width);
+		stage_widths.push(stage_width);
+		var stage_height = jQuery(container).attr("data-stageheight");
+		console.log(stage_height);
+	    stage_heights.push(stage_height);
+		
+		// Get camera attributes
+		var cameraangle = jQuery(container).attr("data-cameraangle");
+	    console.log(cameraangle);
+		var camerafar = jQuery(container).attr("data-camerafar");
+		console.log(camerafar);
+		var camerax = jQuery(container).attr("data-camerax");
+		console.log(camerax);
+		var cameray = jQuery(container).attr("data-cameray");
+		console.log(cameray);
+		var cameraz = jQuery(container).attr("data-cameraz");
+		console.log(cameraz);
+		
+		// Get model files
+		var mtl = jQuery(container).attr("data-mtl");
+		console.log(mtl);
+		var mtl_file = decodeURIComponent(mtl);
+		console.log(mtl_file);
+		var obj = jQuery(container).attr("data-obj");
+		console.log(obj);
+		var obj_file = decodeURIComponent(obj);
+		console.log(obj_file);
+		
+		/* Load model */
+		var mtlLoader = new MTLLoader();
+	    mtlLoader.load(mtl_file, (materials) => {
 	
-	// Camera
-	var SCREEN_WIDTH = stage_width, SCREEN_HEIGHT = stage_height;
-	var VIEW_ANGLE = Number(cameraangle), ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = Number(camerafar);
-	camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-	scene.add(camera);
-	camera.position.set(Number(camerax),Number(cameray),Number(cameraz));	
+	        materials.preload();
 	
-	ambient = new THREE.AmbientLight(0xffffff, 1.0);	
-	scene.add(ambient);
-	// Lighting
-	keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-	keyLight.position.set(-100, 0, 100);
-
-	fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-	fillLight.position.set(100, 0, 100);
-
-	backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-	backLight.position.set(100, 0, -100).normalize();
-
-	scene.add(keyLight);
-	scene.add(fillLight);
-	scene.add(backLight);
+	        var objLoader = new OBJLoader();
+	        objLoader.setMaterials(materials);
+	        objLoader.load(obj_file, function (object) {
+	        
+	        	// Create scene
+				var scene = new THREE.Scene();
+				scenes.push(scene);
+				
+				// Camera
+				var SCREEN_WIDTH = stage_width, SCREEN_HEIGHT = stage_height;
+				var VIEW_ANGLE = Number(cameraangle), ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = Number(camerafar);
+				var camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+				cameras.push(camera);
+				scene.add(camera);
+				camera.position.set(Number(camerax),Number(cameray),Number(cameraz));	
+			
+				var ambient = new THREE.AmbientLight(0xffffff, 1.0);
+				ambients.push(ambient);	
+				scene.add(ambient);
+				// Lighting
+				var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+				keyLights.push(keyLight);
+				keyLight.position.set(-100, 0, 100);
+			
+				var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+				fillLights.push(fillLight);
+				fillLight.position.set(100, 0, 100);
+			
+				var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+				backLights.push(backLight);
+				backLight.position.set(100, 0, -100).normalize();
+			
+				scene.add(keyLight);
+				scene.add(fillLight);
+				scene.add(backLight);
+	            scene.add(object);
 	
-	/* Load model */
-	var mtlLoader = new MTLLoader();
-    mtlLoader.load(mtl_file, (materials) => {
-
-        materials.preload();
-
-        var objLoader = new OBJLoader();
-        objLoader.setMaterials(materials);
-        objLoader.load(obj_file, function (object) {
-            scene.add(object);
-        });
-    });
-
-
-    /* Renderer */
-    
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(stage_width, stage_height);
-    renderer.setClearColor(new THREE.Color("hsl(0, 0%, 10%)"));
-
-    container.appendChild(renderer.domElement);
-
-    /* Controls */
-
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.25;
-          
-    /* Events */
-  
-    window.addEventListener('keydown', function (e) {
-    	e.stopPropagation();
-    	if (e.code === 'KeyL') {
-            lighting = !lighting;
-            if (lighting) {
-                ambient.intensity = 0.25;
-                scene.add(keyLight);
-                scene.add(fillLight);
-                scene.add(backLight);
-            } else {
-                ambient.intensity = 1.0;
-                scene.remove(keyLight);
-                scene.remove(fillLight);
-                scene.remove(backLight);
-            }
-        }	
-    }, false);
-
-    /* Start animation */
-            
-    animate();
+				/* Renderer */
+	    
+			    var renderer = new THREE.WebGLRenderer();
+			    renderer.setPixelRatio(window.devicePixelRatio);
+			    renderer.setSize(stage_width, stage_height);
+			    renderer.setClearColor(new THREE.Color("hsl(0, 0%, 10%)"));
+			    renderers.push(renderer);
+			    container.appendChild(renderer.domElement);
+			                
+	            /* Controls */
+	
+			    var controls = new OrbitControls(camera, renderer.domElement);
+			    controls.enableDamping = true;
+			    controls.dampingFactor = 0.25;
+			    controls_array.push(controls);
+			     
+			    /* Events */
+			  
+			    window.addEventListener('keydown', function (e) {
+			    	e.stopPropagation();
+			    	if (e.code === 'KeyL') {
+			            lighting = !lighting;
+			            if (lighting) {
+			                ambient.intensity = 0.25;
+			                scene.add(keyLight);
+			                scene.add(fillLight);
+			                scene.add(backLight);
+			            } else {
+			                ambient.intensity = 1.0;
+			                scene.remove(keyLight);
+			                scene.remove(fillLight);
+			                scene.remove(backLight);
+			            }
+			        }	
+			    }, false);
+			
+			    /* Start animation */
+				animate();
+	            
+	        });
+	    }); 
+	});
 };
