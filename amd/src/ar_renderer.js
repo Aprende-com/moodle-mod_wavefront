2f09d62799b0e5ce20552b1cfb86f4f5a91ff72e
Ian Wild <ianwild@luminaconsultancy.com>
2022-03-19 16:46:45 +0000

    Fix bug: camera settings incorrect.

--- amd/src/ar_renderer.js
+++ amd/src/ar_renderer.js
@@ -75,7 +75,6 @@ export const init = (stage) => {
 
 	var VIEW_ANGLE = Number(cameraangle), ASPECT = window.innerWidth / window.innerHeight, NEAR = 0.1, FAR = Number(camerafar);
 	var camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-	camera.position.set(Number(camerax),Number(cameray),Number(cameraz));
 
 	const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
 	light.position.set( 0.5, 1, 0.25 );
